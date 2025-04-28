d9222a1ac34682a56cc54b81f0752b26966cf8cf
Finlay <finlay.mitchner@gmail.com>
2025-04-28 11:35:02 -0700

    complete

--- task2.py
+++ task2.py
@@ -6,3 +6,37 @@ The first 2 entry widgets allow the user to enter in the 2 short sides of a righ
 When the button is clicked, calculate the length of the hypotenuse and display it in the 3rd entry widget.
 Any labels you need for instruction are optional.
 """
+import tkinter as tk
+from tkinter import *
+window = tk.Tk()
+
+def run(event):
+    print(event)
+    s1 = float(s1e.get())
+    s2 = float(s2e.get())
+    hyp = (s2**2 + s1**2)**(1/2)
+    hype.delete(0,tk.END)
+    hype.insert(0,hyp)
+
+instruct= tk.Label(window,text = "Enter two short sides of a triangle to find the hypotenuse")
+s1e = tk.Entry(window)
+s2e = tk.Entry(window)
+s1lb = tk.Label(window, text = "Side 1")
+s2lb = tk.Label(window, text = "Side 2")
+hypbt = tk.Button(window, text = "Hypotenuse:")
+hypbt.bind("<Button>", run)
+hype = tk.Entry(window)
+
+
+instruct.pack()
+s1lb.pack()
+s1e.pack()
+s2lb.pack()
+s2e.pack()
+hypbt.pack()
+hype.pack()
+
+window.mainloop()
+
+
+
--- task3.py
+++ task3.py
@@ -10,6 +10,29 @@ import tkinter as tk
 w = tk.Tk()
 w.attributes("-topmost",True)
 
+def run(event):
+    print(event)
+    print(event.widget)
+    n1 = float(e[0].get())
+    n2 = float(e[1].get())
+    print(n1,n2)
+    if event.widget == b[0]:
+        ans = n1*n2
+        print(ans)
+    elif event.widget == b[1]:
+        ans = n1+n2
+    elif event.widget == b[2]:
+        ans = n1-n2
+    elif event.widget == b[3]:
+        ans = n1/n2
+    e[2].config(state = 'normal' )
+    e[2].delete(0,tk.END)
+    e[2].insert(0,ans)
+    e[2].config(state = 'readonly')
+    
+
+
+
 l = []
 l.append( tk.Label(w,text="Number 1"))
 l.append( tk.Label(w,text="Number 2"))
@@ -19,12 +42,17 @@ l.append( tk.Label(w,text="Number Calculator"))
 e = []
 e.append( tk.Entry(w,text=""))
 e.append( tk.Entry(w,text=""))
-e.append( tk.Entry(w,text="answer",state='disabled'))
+e.append( tk.Entry(w,text="answer",state='readonly'))
 b=[]
 b.append(tk.Button(w,text="x"))
 b.append(tk.Button(w,text="+"))
 b.append(tk.Button(w,text="-"))
 b.append(tk.Button(w,text="÷"))
+b[0].bind("<Button>",run)
+b[1].bind("<Button>",run)
+b[2].bind("<Button>",run)
+b[3].bind("<Button>",run)
+
 
 l[2].grid(row=1,column=1,columnspan=4)
 l[0].grid(row=2,column=1,columnspan=2)
